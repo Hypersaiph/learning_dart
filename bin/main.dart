bab22930ba018f56fbe4c4a6a88f1b41520789dd
Hypersaiph <mygvs.mh@gmail.com>
2018-09-01 22:08:44 -0400

    comparisons

--- bin/main.dart
+++ bin/main.dart
@@ -1,35 +1,8 @@
 import 'package:learning_dart/learning_dart.dart' as learning_dart;
 
 main(List<String> arguments) {
-  print('Hello geo, the number you are looking for is: ${learning_dart.calculate()}!');
-  var country = "Bolivia"; //string
-  print(country);
-  /*
-  * Types
-  *
-  * String
-  * Number (integer and double)
-  * Boolean
-  * Losts
-  * maps
-  * .
-  * ..
-  * ...
-  * */
-  String a = 'a';
-  int b = 1;
-  double c = 1.0;
-  bool boo_woo = true;
-  String name = 'James';
-  String lastname = 'Bond';
-  print('My name is $lastname, $name $lastname... witw');
-  int numberOne = 34;
-  int numberTwo = 2;
-  print(numberOne + numberTwo);
-  var result = numberOne + numberTwo;
-  print(result);
-  double pi = 3.14;
-  double gravity = 9.8;
-  print(pi + gravity);
-  print(gravity - pi);
+  //print('Hello geo, the number you are looking for is: ${learning_dart.calculate()}!');
+  //learning_dart.dataTypes();
+  //learning_dart.operations();
+  learning_dart.comparison();
 }
--- lib/learning_dart.dart
+++ lib/learning_dart.dart
@@ -1,3 +1,44 @@
 int calculate() {
   return 6 * 7;
 }
+void dataTypes() {
+  var country = "Bolivia"; //string
+  print(country);
+  /*
+  * Types
+  *
+  * String
+  * Number (integer and double)
+  * Boolean
+  * Losts
+  * maps
+  * .
+  * ..
+  * ...
+  * */
+  String a = 'a';
+  int b = 1;
+  double c = 1.0;
+  bool boo_woo = true;
+  String name = 'James';
+  String lastname = 'Bond';
+  print('My name is $lastname, $name $lastname... witw');
+}
+void operations() {
+  int numberOne = 34;
+  int numberTwo = 2;
+  print(numberOne + numberTwo);
+  var result = numberOne + numberTwo;
+  print(result);
+  double pi = 3.14;
+  double gravity = 9.8;
+  print(pi + gravity);
+  print(gravity - pi);
+  print(4 % 2);
+}
+void comparison() {
+  print(4 == 2);
+  print(4 == 4);
+  print(!(4 == 4));
+  print(4 != 4);
+}
